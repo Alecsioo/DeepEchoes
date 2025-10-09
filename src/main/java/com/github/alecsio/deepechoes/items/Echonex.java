ce00636d941f50d5c87de1f79955ac321d546b65
Alecsioo <141121073+Alecsioo@users.noreply.github.com>
2025-10-09 18:25:42 +0200

    Initial scaffolding

---
+++ src/main/java/com/github/alecsio/deepechoes/items/Echonex.java
@@ -0,0 +1,10 @@
+package com.github.alecsio.deepechoes.items;
+
+import net.minecraft.world.item.Item;
+
+public class Echonex extends Item {
+
+    public Echonex(Properties properties) {
+        super(properties);
+    }
+}
